638fa0e12c4f92a8d4eb12ffbe405166cb5d9255
eunseo <92281453+les0498@users.noreply.github.com>
2024-09-05 22:53:15 +0900

    Update App.jsx

--- my-gpt-diary/src/App.jsx
+++ my-gpt-diary/src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  // 변수 설정
+  const [count, setCount] = useState(0);
+  const [userInput, setUserInput] = useState("");
+
+  // 함수
+  const handleClickPlus = () => {
+    setCount(count + 1);
+  }; 
+  const handleClickMinus = () => {
+    setCount(count - 1);
+  }
+  const handleUserInput = (e) => {
+    setUserInput(e.target.value);
+  };
+  const handleEnter = (e) => {
+    if (e.key === "Enter") {
+      setUserInput("");
+      const num = Number(userInput);
+      if (Number.isInteger(num)) setCount(num);
+    }
+  };
 
   return (
     <>
+      <div>current count : {count} </div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div>count value input : </div>
+        <input 
+          value={userInput}
+          onChange={handleUserInput}
+          onKeyDown={handleEnter}
+          ></input>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <div>buttons : </div>
+        <button onClick={handleClickPlus}>+</button>
+        <button onClick={handleClickMinus}>-</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
